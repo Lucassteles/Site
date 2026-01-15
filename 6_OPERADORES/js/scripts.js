b4532bb636c6359bf012484a397d6501dbf5b3f3
Lucas Teles <lucas_teles500@hotmail.com>
2026-01-15 19:09:22 -0300

    javascript

---
+++ 6_OPERADORES/js/scripts.js
@@ -0,0 +1,40 @@
+// 1- Number 
+//console.log(typeof 2); 
+//console.log(typeof 5.14 ); 
+//console.log(typeof -127);
+ 
+
+// 2- OPs. 
+//console.log(2 + 4 ); 
+//console.log( 10 - 5 ); 
+//console.log(5 * 4); 
+//console.log(10 / 2); 
+
+// 3- Special Numbers 
+//console.log(typeof Infinity); 
+
+//console.log(typeof -Infinity); 
+
+//console.log(12 * "asd"); 
+
+//console.log(typeof NaN); 
+
+//4-Strings 
+//console.log("Um texto"); 
+//console.log('Mais um texto'); 
+//console.log('13'); 
+
+//console.log(typeof "Um texto"); 
+//console.log(typeof 'Mais um texto'); 
+ 
+
+//5-Símbolo especiais em string 
+//console.log("Testando a \n quebra de linha"); 
+
+//console.log("Espaçamento \t de tab") 
+
+
+//6- Concatenação 
+console.log("Oi," + "tudo" + "bem ? "); 
+
+console.log('Testando' + 'com' + 'crase!'); 
