ed6818011e8eac0a9a4cf531c89e4d250223b086
Lucas Teles <lucas_teles500@hotmail.com>
2026-01-16 18:25:36 -0300

    Atualiza scripts.js

--- 6_OPERADORES/js/scripts.js
+++ 6_OPERADORES/js/scripts.js
@@ -35,6 +35,83 @@
 
 
 //6- Concatenação 
-console.log("Oi," + "tudo" + "bem ? "); 
+//console.log("Oi," + "tudo" + "bem ?"); 
 
-console.log('Testando' + 'com' + 'crase!'); 
+//console.log(`Testando` + `com` + `crase!`); 
+ 
+
+
+//7- Template Strings 
+//console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+
+//console.log(`podemos executar qualquer coisa aqui ${console.log("teste")}`); 
+
+//8 - boolean 
+//console.log(true); 
+
+//console.log( 5 > 20); 
+
+//console.log(30 > 10); 
+
+//console.log(typeof false); 
+
+// 9 - Comparações 
+//console.log( 5<= 5); 
+
+//console.log( 5 < 5); 
+
+//console.log( 10 == 10); 
+
+
+//console.log (10 == 9); 
+ 
+//console.log(10 != 9); 
+
+// 10- Idêntico 
+//console.log(9 == "9"); 
+
+//console.log(9 === "9");  
+
+//console.log(9 !="9"); 
+
+//console.log (9 !== "9"); 
+
+
+//11- Operadores lógicos 
+
+console.log(true && true) 
+
+console.log( true && false); 
+
+console.log( 5 > 2 && < 10); 
+
+console.log( 5 > 2 && "Matheus" === 1); 
+
+console.log( 5 > 2 && || "Matheus" === 1); 
+ 
+console.log( 5 > 2 && || 5 > 100); 
+
+console.log(!true); 
+
+console.log(!5 > 2); 
+
+//12 - empty values 
+console.log(typeof null, typeof undefined); 
+
+console.log(null === undefined); 
+
+console.log(null == undefined); 
+
+console.log(null == false); 
+
+console.log(undefined == false);  
+
+// 13 - mudança de tipos 
+console.log( 5 * null); 
+
+console.log("teste" * "opa"); 
+
+console.log("10" + 1); 
+ 
+console.log("10" - 1);
